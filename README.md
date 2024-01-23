19adc61ddc30568054c62748ea0ae3925683de2a
Neel Dave <neeldeveloper97@gmail.com>
2024-01-23 15:10:10 +0530

    Code cleaning

--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 - yarn install
 - npx pod-install ios -> For IOS
 
-- yarn android -> Android
-- yarn ios -> IOS
+- yarn android -> Run on Android
+- yarn ios -> Run on IOS
 
 That's it!
 
--- src/redux/slices/test.slice.ts
+++
@@ -1,9 +0,0 @@
-import {createSlice} from '@reduxjs/toolkit';
-
-const testSlice = createSlice({
-  name: 'Test',
-  initialState: {},
-  reducers: {},
-});
-
-export default testSlice.reducer;
